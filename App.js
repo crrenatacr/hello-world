df5b1d19a98b2f09adc5a86ec516f4aacb05b85d
Renata C R <152975781+crrenatacr@users.noreply.github.com>
2024-10-27 01:09:23 +0200

    FirstColors

--- App.js
+++ App.js
@@ -4,18 +4,31 @@ import { StyleSheet, Text, View } from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello World !</Text>
+      <Text style={styles.blue}>Hello World!</Text>
+      <Text style={styles.bigRed}>How are you?</Text>
+      <Text style={[styles.blue, styles.bigRed]}>I'm feeling blue!</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+
+  blue: {
+    color: "blue",
+    fontWeight: "600",
+  },
+
+  bigRed: {
+    color: "red",
+    fontSize: 30,
+  },
+
 });
 
 export default App;
