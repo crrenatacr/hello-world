f5a7429b61f5a00489090158a89f2edc48145677
Renata C R <152975781+crrenatacr@users.noreply.github.com>
2024-10-27 21:26:11 +0100

    FirstTrials

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react"; // Import useState for managing state
+import {
+  StyleSheet,
+  View,
+  Text,
+  Alert,
+  TouchableOpacity,
+  TextInput, 
+  ScrollView,
+} from "react-native"; // Import necessary components from react-native
 
 const App = () => {
+  const [text, setText] = useState(""); // State to store user input text
+
+  // Alert the user input (`text` state's value)
+  const alertMyText = () => {
+    Alert.alert(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.blue}>Hello World!</Text>
-      <Text style={styles.bigRed}>How are you?</Text>
-      <Text style={[styles.blue, styles.bigRed]}>I'm feeling blue!</Text>
+      <TextInput
+        style={styles.textInput}
+        value={text}
+        onChangeText={setText}
+        placeholder="Type Something Here"
+      />
+      <Text style={styles.textDisplay}>You wrote: {text}</Text>
+
+      {/* Replace Button with TouchableOpacity */}
+      <TouchableOpacity style={styles.button} onPress={alertMyText}>
+        <Text style={styles.buttonText}>Press Here</Text>
+      </TouchableOpacity>
+
+      <ScrollView>
+        <Text style={{ fontSize: 110 }}>
+          This text is so big! And so long! You have to scroll!
+        </Text>
+      </ScrollView>
     </View>
   );
 };
@@ -14,21 +44,31 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#bfbcc2",
   },
-
-  blue: {
-    color: "blue",
-    fontWeight: "600",
+  textInput: {
+    width: "88%",
+    borderWidth: 1,
+    height: 50,
+    padding: 10,
+    marginTop: 100, // Add margin to move it down
   },
-
-  bigRed: {
-    color: "red",
-    fontSize: 30,
+  textDisplay: {
+    height: 50,
+    lineHeight: 50,
   },
-
+  button: {
+    marginTop: 20, // Add some margin to separate the button from other elements
+    padding: 10, // Add padding inside the button
+    backgroundColor: "#2f4254", // Button background color
+    borderRadius: 5, // Rounded corners
+  },
+  buttonText: {
+    color: "white", // Set the font color for the button text
+    textAlign: "center", // Center the text
+  },
 });
 
-export default App;
+export default App; // Export the App component
